f54133694a2c69a2497cc6c7401c14c4e4fa5c23
slynchoO <62185813+slynchoO@users.noreply.github.com>
2020-10-15 14:28:24 +0300

    Add files via upload

---
+++ 7.nXn-matrix.js
@@ -0,0 +1,12 @@
+function matrix(n) {
+    let line = ' ';
+    for(let i = 0; i < n; i++) {
+        for(let k = 0; k < n; k++) {
+            line += n + ' ';
+        }
+    console.log(line);
+    line = ' ';
+    }
+}
+
+matrix(3)
